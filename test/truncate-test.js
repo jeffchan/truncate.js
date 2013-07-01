98d8e166055f4f2345666bac96e61c96342c4cf5
Jeff Chan <jeffchan@mit.edu>
2013-07-01 15:36:25 +0200

    Add test

--- test/truncate-test.js
+++ test/truncate-test.js
@@ -22,8 +22,8 @@ describe('truncatejs', function () {
     assert.equal(this.div.clientHeight, 12);
   });
 
-  it('truncate v2', function () {
-    this.div.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  it('truncate four lines', function () {
+    this.div.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor';
     this.div.style.width = '50px';
     this.div.style['font-size'] = '12px';
     this.div.style['line-height'] = '12px';
@@ -34,6 +34,23 @@ describe('truncatejs', function () {
     });
 
     assert.equal(this.div.clientHeight, 48);
+  });
+
+  it('truncate two lines, leaving comments and child nodes intact', function () {
+    this.div.innerHTML = 'members, friends, adversaries, competitors, and colleagues--<!--- test --><em>Walter Isaacson</em>';
+    this.div.style.width = '230px';
+    this.div.style['font-size'] = '12px';
+    this.div.style['line-height'] = '12px';
+
+    var truncated = new Truncate(this.div, {
+      lines: 2,
+      lineHeight: 12,
+      showMore: '',
+      showLess: ''
+    });
+
+    assert.equal(this.div.clientHeight, 24);
+    assert.equal(this.div.innerHTML, 'members, friends, adversaries, competitors, and colleagues--<!--- test -->');
   });
 });
 
@@ -50,6 +67,6 @@ describe('truncatejs', function () {
   with comments
 
 
-      <div id="test" style="width: 330px; border: 1px solid black;">members, friends, adversaries, competitors, and colleagues--<!--- test --><em>Walter Isaacson</em> has written a riveting story of the roller-coaster life and searingly intense personality of a creative entrepreneur whose passion for perfection and ferocious drive revolutionized six industries: personal computers, animated movies, music, phones, tablet computing, and digital publishing.
+      <div id="test" style="width: 330px; border: 1px solid black;"> has written a riveting story of the roller-coaster life and searingly intense personality of a creative entrepreneur whose passion for perfection and ferocious drive revolutionized six industries: personal computers, animated movies, music, phones, tablet computing, and digital publishing.
 At a time when America is seeking ways to sustain its innovative edge, and when societies around the world are trying to build digital-age economies, Jobs stands as the ultimate icon of inventiveness and applied imagination. He knew that the best way to create value in the twenty-first century was to connect creativity with technology. He built a company where leaps of the imagination were combined with remarkable feats of engineering.</div>
 */
