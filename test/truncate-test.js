8ca0ec99424e96ee98237517dc695d180c38ea7f
Jeff Chan <jeffchan@mit.edu>
2013-07-10 19:24:02 +0200

    Update test doc

--- test/truncate-test.js
+++ test/truncate-test.js
@@ -66,7 +66,8 @@ describe('truncatejs', function () {
 
   with comments
 
+  nested tags
 
-      <div id="test" style="width: 330px; border: 1px solid black;"> has written a riveting story of the roller-coaster life and searingly intense personality of a creative entrepreneur whose passion for perfection and ferocious drive revolutionized six industries: personal computers, animated movies, music, phones, tablet computing, and digital publishing.
+  <div id="test" style="width: 330px; border: 1px solid black;"> has written a riveting story of the roller-coaster life and searingly intense personality of a creative entrepreneur whose passion for perfection and ferocious drive revolutionized six industries: personal computers, animated movies, music, phones, tablet computing, and digital publishing.
 At a time when America is seeking ways to sustain its innovative edge, and when societies around the world are trying to build digital-age economies, Jobs stands as the ultimate icon of inventiveness and applied imagination. He knew that the best way to create value in the twenty-first century was to connect creativity with technology. He built a company where leaps of the imagination were combined with remarkable feats of engineering.</div>
 */
